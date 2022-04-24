b65678935138cf5ac6dc5c7a7e84e9e84245431e
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2022-04-24 11:35:59 +0530

    Update warns.py

--- Yuriko/modules/warns.py
+++ Yuriko/modules/warns.py
@@ -509,7 +509,7 @@ __help__ = """
 
 ✗ /strongwarn - `<on/yes/off/no> If set to on, exceeding the warn limit will result in a ban. Else, will just punch.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: BᴏᴛDᴜɴɪʏᴀ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: RᴇɢᴀʟᴛᴏsBᴏᴛᴢ!*
 """
 
 __mod_name__ = "Wᴀʀɴɪɴɢ"
